93edb6335c2031b2e33306b51bfdc3bb6024a060
Petridis, Ross (NCMI, Hobart) <Ross.Petridis@csiro.au>
2025-11-21 09:51:01 +1100

    remove error and exit 1 and reduce to serious warning so container still starts for further trouble shooting in ECS

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,14 +2,12 @@
 set -e
 
 if [ ! -d $OUTPUTS_PATH ]; then
-    echo "ERROR: Output directory mount is required but not found at: $OUTPUTS_PATH. Customise this path with the OUTPUTS_PATH environment variable."
-    exit 1
+    echo "!! WARNING !!: Output directory mount is required but not found at: $OUTPUTS_PATH. Program will likely FAIL! Customise this path with the OUTPUTS_PATH environment variable."
 fi
 
 if [ ! -d $RME_PATH ]; then
-    echo "ERROR: RME directory mount is required but not found at: $RME_PATH. Customise this path with the RME_PATH environment variable."
+    echo "!! WARNING !!: RME directory mount is required but not found at: $RME_PATH. Program will likely FAIL! Customise this path with the RME_PATH environment variable."
     echo "IMPORTANT NOTE : This RME must have the reefmod_gbr.gpkg inside /path/to/rme/data_files/region. See step 1 of https://open-aims.github.io/ReefModEngine.jl/v1.4.1/getting_started#Pre-initialization-setup. Step 2 is not required in this image."
-    exit 1
 fi
 
 
